57e3d7cf9eacd1535d7e4db6a0ab9b3829042a04
Xi Guo <37179170+xi-guo-0@users.noreply.github.com>
2025-04-15 08:49:43 +0800

    refactor: improve 2179 time efficiency

--- racket/2179.rkt
+++ racket/2179.rkt
@@ -3,34 +3,30 @@
 
 (define/contract (good-triplets nums1 nums2)
   (-> (listof exact-integer?) (listof exact-integer?) exact-integer?)
-
   (let* ([n (length nums1)]
-         [pos1 (make-vector n 0)]
-         [pos2 (make-vector n 0)])
+         [pos1 (make-vector n 0)])
     (for ([(v i) (in-indexed nums1)])
       (vector-set! pos1 v i))
-    (for ([(v i) (in-indexed nums2)])
-      (vector-set! pos2 v i))
-    (let ([combined-pos (for/list ([v (in-list nums1)])
-                          (vector-ref pos2 v))])
-      (let ([ft (make-vector (add1 n) 0)])
-        (let loop ([i 0]
-                   [res 0])
-          (if (= i n)
-              res
-              (let* ([pos (list-ref combined-pos i)]
-                     [left (let loop-query ([idx (add1 pos)]
-                                            [sum 0])
-                             (if (<= idx 0)
-                                 sum
-                                 (let ([lsb (bitwise-and idx (- idx))])
-                                   (loop-query (- idx lsb) (+ sum (vector-ref ft idx))))))]
-                     [_ (let loop-update ([idx (add1 pos)])
-                          (when (< idx (vector-length ft))
-                            (vector-set! ft idx (add1 (vector-ref ft idx)))
-                            (loop-update (+ idx (bitwise-and idx (- idx))))))]
-                     [right (- (- (sub1 n) pos) (- i left))])
-                (loop (add1 i) (+ res (* left right))))))))))
+    (let* ([combined-pos (for/vector ([v (in-list nums2)])
+                           (vector-ref pos1 v))]
+           [ft (make-vector (add1 n) 0)])
+      (let loop ([i 0]
+                 [res 0])
+        (if (= i n)
+            res
+            (let* ([pos (vector-ref combined-pos i)]
+                   [left (let q-loop ([idx (add1 pos)]
+                                      [sum 0])
+                           (if (<= idx 0)
+                               sum
+                               (let ([lsb (bitwise-and idx (- idx))])
+                                 (q-loop (- idx lsb) (+ sum (vector-ref ft idx))))))]
+                   [_ (let u-loop ([idx (add1 pos)])
+                        (when (< idx (vector-length ft))
+                          (vector-set! ft idx (add1 (vector-ref ft idx)))
+                          (u-loop (+ idx (bitwise-and idx (- idx))))))]
+                   [right (- (- (sub1 n) pos) (- i left))])
+              (loop (add1 i) (+ res (* left right)))))))))
 
 (module+ test
   (require rackunit)
